a5755568bf846a8cd18030892394fc0b47d48df8
JarbasAi <jarbasai@mailfence.com>
2024-01-07 03:03:22 +0000

    refactor/ocp

--- __init__.py
+++ __init__.py
@@ -1,21 +1,18 @@
 from os.path import join, dirname
 
-from ovos_plugin_common_play.ocp import MediaType, PlaybackType
-from ovos_utils.parse import fuzzy_match, MatchStrategy
-from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill, \
-    ocp_search, ocp_featured_media
 from reddit_movies import RedditCartoons
+
+from ovos_utils.ocp import MediaType, PlaybackType
+from ovos_workshop.decorators.ocp import ocp_search, ocp_featured_media
+from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill
 
 
 class RedditCartoonsSkill(OVOSCommonPlaybackSkill):
-
-    def __init__(self):
-        super().__init__("RedditCartoons")
+    def __init__(self, *args, **kwargs):
         self.skill_icon = join(dirname(__file__), "ui", "logo.png")
-        self.supported_media = [MediaType.CARTOON,
-                                MediaType.GENERIC,
-                                MediaType.VIDEO]
+        self.supported_media = [MediaType.CARTOON]
         self.reddit = RedditCartoons()
+        super().__init__(*args, **kwargs)
 
     def initialize(self):
         # get your own keys! these might stop working any time
@@ -30,55 +27,56 @@ class RedditCartoonsSkill(OVOSCommonPlaybackSkill):
                 client=self.settings["praw_client"],
                 secret=self.settings["praw_secret"])
 
-        self.schedule_event(self._scrap_reddit, 1)
+        self._scrap_reddit()
+        self.register_ocp_keyword(MediaType.CARTOON,
+                                  "cartoon_streaming_provider",
+                                  ["Reddit Cartoons", "Reddit"])
 
     def _scrap_reddit(self, message=None):
+        movies = []
+        for v in self.reddit.get_cached_entries():
+            t = v["title"].split(" (")[0].replace("COMPLETE", "")
+            if '"' in t:
+                t = t.split('"')[1]
+            movies.append(t.strip())
         for v in self.reddit.scrap():
-            pass  # TODO Some validation ?
+            t = v["title"].split(" (")[0].replace("COMPLETE", "")
+            if '"' in t:
+                t = t.split('"')[1]
+            movies.append(t.strip())
+        self.register_ocp_keyword(MediaType.CARTOON,
+                                  "cartoon_name", movies)
         self.schedule_event(self._scrap_reddit, 3600)  # repeat every hour
-
-    def calc_score(self, phrase, match, base_score=0):
-        # check for forbidden words in title
-        if self.voc_match(match["title"].lower(), "blacklist"):
-            return 0
-
-        score = 100 * fuzzy_match(phrase.lower(), match["title"].lower(),
-                                  strategy=MatchStrategy.TOKEN_SET_RATIO)
-        return min(100, base_score + score)
-
-    def parse_media_type(self, phrase, media_type):
-        score = 0
-        if self.voc_match(phrase, "reddit"):
-            score += 10
-            phrase = self.remove_voc(phrase, "reddit")
-        if self.voc_match(phrase, "cartoons") or \
-                media_type == MediaType.CARTOON:
-            score += 50
-            phrase = self.remove_voc(phrase, "cartoons")
-        return score, phrase
 
     @ocp_search()
     def search_reddit(self, phrase, media_type):
-        if self.voc_match(phrase, "cartoons") or media_type == MediaType.CARTOON:
-            base_score, phrase = self.parse_media_type(phrase, media_type)
-            # search cached documentary database (updated hourly)
+        base_score = 15 if media_type == MediaType.MOVIE else 0
+        entities = self.ocp_voc_match(phrase)
+
+        title = entities.get("cartoon_name")
+        skill = "cartoon_streaming_provider" in entities  # skill matched
+
+        base_score += 30 * len(entities)
+        if title:
+            base_score += 30
+            # search cached database (updated hourly)
             for v in self.reddit.get_cached_entries():
-                score = self.calc_score(phrase, v, base_score=base_score)
-                if score < 20:
-                    continue
-                # return as a video result (single track dict)
-                yield {
-                    "match_confidence": score,
-                    "media_type": MediaType.CARTOON,
-                    #  "length": v.length * 1000,
-                    "uri": "youtube//" + v["url"],
-                    "playback": PlaybackType.VIDEO,
-                    "image": v.get("thumbnail") or self.skill_icon,
-                    "bg_image": v.get("thumbnail") or self.skill_icon,
-                    "skill_icon": self.skill_icon,
-                    "title": v["title"],
-                    "skill_id": self.skill_id
-                }
+                if title.lower() in v["title"].lower():
+                    # return as a video result (single track dict)
+                    yield {
+                        "match_confidence": base_score,
+                        "media_type": MediaType.CARTOON,
+                        #  "length": v.length * 1000,
+                        "uri": "youtube//" + v["url"],
+                        "playback": PlaybackType.VIDEO,
+                        "image": v.get("thumbnail") or self.skill_icon,
+                        "bg_image": v.get("thumbnail") or self.skill_icon,
+                        "skill_icon": self.skill_icon,
+                        "title": v["title"],
+                        "skill_id": self.skill_id
+                    }
+        if skill:
+            yield self.featured_media()
 
     @ocp_featured_media()
     def featured_media(self):
@@ -96,5 +94,10 @@ class RedditCartoonsSkill(OVOSCommonPlaybackSkill):
         } for v in self.reddit.get_cached_entries()]
 
 
-def create_skill():
-    return RedditCartoonsSkill()
+if __name__ == "__main__":
+    from ovos_utils.messagebus import FakeBus
+
+    s = RedditCartoonsSkill(bus=FakeBus(), skill_id="t.fake")
+    for r in s.search_reddit("Steamboat Willie", MediaType.MOVIE):
+        print(r)
+        # {'match_confidence': 75, 'media_type': <MediaType.CARTOON: 21>, 'uri': 'youtube//https://www.youtube.com/watch?v=9rldp02XzDA', 'playback': <PlaybackType.VIDEO: 1>, 'image': 'https://github.com/OpenVoiceOS/ovos-ocp-audio-plugin/raw/master/ovos_plugin_common_play/ocp/res/ui/images/ocp.png', 'bg_image': 'https://github.com/OpenVoiceOS/ovos-ocp-audio-plugin/raw/master/ovos_plugin_common_play/ocp/res/ui/images/ocp.png', 'skill_icon': 'https://github.com/OpenVoiceOS/ovos-ocp-audio-plugin/raw/master/ovos_plugin_common_play/ocp/res/ui/images/ocp.png', 'title': 'Steamboat Willie (1928)', 'skill_id': 't.fake'}
