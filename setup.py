e17329d1ace3536ff8ebbd5176f0da9c167d19e7
JarbasAi <jarbasai@mailfence.com>
2024-01-09 21:15:54 +0000

    drop "ui" folder

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author_email='jarbasai@mailfence.com',
     license='Apache-2.0',
     package_dir={"skill_reddit_cartoons": ""},
-    package_data={'skill_reddit_cartoons': ['locale/*', 'ui/*']},
+    package_data={'skill_reddit_cartoons': ['locale/*', 'res/*']},
     packages=['skill_reddit_cartoons'],
     include_package_data=True,
     install_requires=["ovos_workshop~=0.0.5a1"],
